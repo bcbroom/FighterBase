4ac14a22ac4de11b7e90c41fa7a348de2128fe2d
Brian Broom <brian.broom@gmail.com>
2015-12-17 21:35:18 -0500

    Add heartbeat method to level and exchange that checks the API and the exchange are up

--- lib/fighter_base/exchange.rb
+++ lib/fighter_base/exchange.rb
@@ -1,5 +1,21 @@
-module FighterBase
-    class Exchange
+require 'level'
+require 'httparty'
 
+module FighterBase
+    @attr_reader :venue_symbol, :level
+
+    class Exchange
+        def initialize(venue_symbol, level)
+            @venue_symbol = venue_symbol
+            @level = level
+        end
+
+        def heartbeat
+            response = HTTParty.get("https://api.stockfighter.io/ob/api/venues/#{venue_symbol}/heartbeat")
+            ok = response.parsed_response["ok"] rescue false
+
+            raise "World is on fire!" unless ok
+            puts "lump lump, lump lump" if ok
+        end
     end
 end
--- lib/fighter_base/level.rb
+++ lib/fighter_base/level.rb
@@ -1,12 +1,23 @@
 require 'exchange'
+require 'httparty'
 
 module FighterBase
     class Level
         @attr_reader api_key, exchange
 
-        def initialize(api_key)
+        def initialize(api_key, venue_symbol)
             @api_key = api_key
-            @exchange = Exchange.new(self)
+            @exchange = Exchange.new(venue_symbol, self)
+        end
+
+        def hearbeat
+            response = HTTParty.get("https://api.stockfighter.io/ob/api/hearbeat")
+            ok = response.parsed_response["ok"] rescue false
+
+            raise "World is on fire!" unless ok
+            puts "lump lump, lump lump" if ok
+
+            end
         end
     end
 end
